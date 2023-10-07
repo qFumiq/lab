095d93c0121241f718efab14d12a7c7da2577f10
Магомед Бузуртанов <qzombieq_buzurtanov@mail.ru>
2023-10-07 13:25:53 +0300

    Код был изменен

--- hello.pas
+++ hello.pas
@@ -1,3 +1,16 @@
 ﻿begin
-  print('Hello world');
+  var H := ReadInteger('Который час?');
+  if (h > 23) or (h < 0) then
+    print('В сутках только 24 часа')
+  else
+  begin
+    if ( 3 < h ) and (h < 11) then
+      print('Доброе утро, мир!');
+    if (10 < h) and (h < 17) then
+      print('Добрый день, мир!');
+    if (16 < h) and (h < 23) then
+      print('Добрый вечер, мир!')
+    else
+      print('Доброй ночи, мир')
+  end;
 end.

371558b57524a54b4743999c35a21731d5382d48
Магомед Бузуртанов <qzombieq_buzurtanov@mail.ru>
2023-10-07 14:39:16 +0300

    Программа добавлена

---
+++ timer.pas
@@ -0,0 +1,16 @@
+﻿var
+  hour, minutes, seconds, all : Integer;
+begin
+  
+  Print('Введите количество часов:');
+  Read(hour);
+  
+  Print('Введите количество минут:');
+  Read(minutes);
+  
+  Print('Введите количество секунд:');
+  Read(seconds);
+
+  all := (Hour * 3600) + (Minutes * 60) + Seconds;
+  print(all);
+end.
